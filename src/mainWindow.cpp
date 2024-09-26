79a2116ea11f9784edff56aee70694f84d6a39ff
PozhiloyPumba <ivanov.in@phystech.edu>
2024-09-27 00:19:31 +0300

    added switch button

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -9,11 +9,21 @@ MainWindow::MainWindow(QWidget *parent)
     setWindowTitle("APP_NAME");
     setCentralWidget(ui->Hueim);
     setWindowFlags(Qt::Window);
-    connect(ui->pushButton, &QAbstractButton::clicked, this, 
+    ui->tristate->setCheckable(true);
+    connect(ui->pushButton, &QAbstractButton::clicked,
         [this](){
             const auto &textToPush = ui->textInput->toPlainText();
             qDebug() << "Text to push:" << textToPush;
-            ui->textOutput->setText(ui->textInput->toPlainText());
+            ui->textOutput->setText(textToPush);
+            if(ui->tristate->isChecked()) {
+                qDebug() << "Unchecked tristate clown button";
+                ui->tristate->setChecked(false);
+            }
+        }
+    );
+    connect(ui->tristate, &QPushButton::clicked, 
+        [this](bool) {
+            ui->tristate->setChecked(true);
         }
     );
 }
