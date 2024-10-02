7cb3511a3bc17480d161e517594733557dd214b6
PozhiloyPumba <ivanov.in@phystech.edu>
2024-10-02 23:34:26 +0300

    refactored structure

---
+++ second_task/src/main.cpp
@@ -0,0 +1,10 @@
+#include "mainWindow.hpp"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    MainWindow mainWindow;
+    mainWindow.show();
+    return app.exec();
+}
